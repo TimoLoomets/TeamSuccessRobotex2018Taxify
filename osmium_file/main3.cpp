9d2aab32a0a97a3a27706be6fd73b83ebfd77820
TimoLoomets <timo@robootika.com>
2018-07-06 21:22:19 +0300

    Fifth Osmium commit

--- osmium_file/main3.cpp
+++ osmium_file/main3.cpp
@@ -19,6 +19,48 @@
 
 std::map<osmium::Location, std::map<osmium::Location, double> > road_graph;
     
+void simplify_node(std::pair<osmium::Location, std::map<osmium::Location, double> > start_node){
+	std::cout << "c1" << "\n";
+	std::vector<std::pair<osmium::Location, double> > end_nodes;  
+	std::cout << "c2" << "\n";
+	for(auto end_node : start_node.second){
+		std::cout << "c3" << "\n";
+		end_nodes.push_back(end_node);
+		std::cout << "c4" << "\n";
+	}
+	if(end_nodes.size() == 2){
+		std::cout << "c5" << "\n";
+		double distance = end_nodes[0].second + end_nodes[1].second;
+		std::cout << "c6" << "\n";
+		osmium::Location a = end_nodes[0].first;
+		std::cout << "c6.0" << "\n";
+		osmium::Location b = end_nodes[1].first;
+		std::cout << "c6.1" << "\n";
+		std::cout << a << b << "\n";
+		std::cout << "c6.2" << "\n";
+		for(auto elem : road_graph[end_nodes[0].first]){
+			std::cout << elem.first << " : " << elem.second;
+		}
+		std::cout << "c6.3" << "\n";
+//		road_graph[end_nodes[0].first][end_nodes[1].first] = distance;
+
+		std::map<osmium::Location, double> temp;
+		temp.insert(std::make_pair(end_nodes[1].first, distance));
+		
+		road_graph.insert(std::make_pair(end_nodes[0].first, temp));
+		std::cout << "c7" << "\n";
+		road_graph[end_nodes[1].first][end_nodes[0].first] = distance;
+		std::cout << "c8" << "\n";
+		road_graph.erase(start_node.first);
+		std::cout << "c9" << "\n";
+		simplify_node(std::make_pair(end_nodes[0].first, road_graph[end_nodes[0].first]));
+		std::cout << "c10" << "\n";
+		simplify_node(std::make_pair(end_nodes[1].first, road_graph[end_nodes[1].first]));
+		std::cout << "c11" << "\n";
+	}
+}
+    
+    
 
 class MyHandler : public osmium::handler::Handler {
 public:
@@ -33,11 +75,11 @@ public:
 		    	} 
 		    }
 		    if (road.size() != 0){
-		    	std::cout << "way " << way.id() << '\n';
-		    	for(int i=0; i<road.size()-1; i++){//const auto& n : road
+		    	//std::cout << "way " << way.id() << '\n';
+		    	for(int i=0; i<road.size()-1; i++){
 		    		osmium::NodeRef& node0 = road[i];
 		    		osmium::NodeRef& node1 = road[i+1];
-		    		std::cout << "\t" << node0.ref() << ": " << node0.lon() << ", " << node0.lat() << '\n';
+		    		//std::cout << "\t" << node0.ref() << ": " << node0.lon() << ", " << node0.lat() << '\n';
 					
 					road_graph[node0.location()][node1.location()] = haversine_distance(node0.lon(), node0.lat(), node1.lon(), node1.lat());
 					road_graph[node1.location()][node0.location()] = haversine_distance(node0.lon(), node0.lat(), node1.lon(), node1.lat());
@@ -49,9 +91,6 @@ public:
 
 
 int main() {
-	//std::cout << "Hello world!" << "\n";
-	//std::cout << haversine_distance(58.5962, 24.9571, 58.5962, 24.9574) << "\n";
-	
     std::string input_file = "estonia-latest.osm.pbf";
     
     
@@ -69,9 +108,17 @@ int main() {
     reader.close();
     
     for(auto start_node : road_graph){
+    	simplify_node(start_node);
+    }
+    
+    int counter = 0;
+    
+    for(auto start_node : road_graph){
     	std::cout << start_node.first << "\n";
+    	counter++;
     	for(auto end_node : start_node.second){
     		std::cout << "\t" << end_node.first << " - " << end_node.second << "\n";
     	}
     }
+    std::cout << counter << "\n";
 }
