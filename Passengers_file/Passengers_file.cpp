693c53d1e6f2e56113833c711278e8f8995ee3d6
EnricoVompa <40790387+EnricoVompa@users.noreply.github.com>
2018-07-14 13:11:20 +0300

    Update Passengers_file.cpp

--- Passengers_file/Passengers_file.cpp
+++ Passengers_file/Passengers_file.cpp
@@ -1,5 +1,5 @@
 
-
+#include "stdafx.h"
 #include "passenger_info.h" 
 #include <iostream>
 #include <fstream>
@@ -23,8 +23,6 @@ int lines = 420001; // amount of lines 420001
 
 string sissetulev_rida;
 
-vector < passenger_data > passengers; //teen uue vektori
-
 string most_of_data;
 
 string time_of_year;
@@ -47,6 +45,15 @@ string str_minut;
 
 string str_sekund;
 
+string string_start_lat;
+
+string string_start_lng;
+
+string string_end_lat;
+
+string string_end_lng;
+
+string string_ride_value;
 
 int amount_of_days;
 
@@ -62,53 +69,58 @@ int minut;
 
 double sekund;
 
-int passenger_information() {
+int timezone = 2;
+
+
+
+vector <passenger_data> passengers() {
+
+
+	vector <passenger_data> passengers_return;
 
 	ifstream inFile;
 	inFile.open("robotex2.csv"); //avan faili
-	ifstream ifs("robotex2.csv"); //analüüsib seda faili
+	ifstream ifs("robotex2.csv"); //analÃ¼Ã¼sib seda faili
 	ifs >> sissetulev_rida;
 
-	for (int i = 0; i < lines; ++i) { //vaatab read läbi(420000 tk)
+	for (int i = 0; i < lines; ++i) { //vaatab read lÃ¤bi(420000 tk)
 
 		passenger_data passenger;
 
-		ifs >> sissetulev_rida; //string >> siin saan kätte kuupäeva
+		ifs >> sissetulev_rida; //string >> siin saan kÃ¤tte kuupÃ¤eva
 		time_of_year = sissetulev_rida;
 
-		ifs >> sissetulev_rida; //string >> siin saan kätte start_time,start_lat,start_lng,end_lat,end_lng,ride_value
+		ifs >> sissetulev_rida; //string >> siin saan kÃ¤tte start_time,start_lat,start_lng,end_lat,end_lng,ride_value
 		most_of_data = sissetulev_rida;
 
 		start_time =
-
-
 			sissetulev_rida.substr(0, sissetulev_rida.find(",", 0));
 
 		sissetulev_rida = sissetulev_rida.substr(sissetulev_rida.find(",", 0) + 1, sissetulev_rida.length()); // new string
 
-		passenger.start_lat =
+		string_start_lat =
 			sissetulev_rida.substr(0, sissetulev_rida.find(",", 0));
-
+		passenger.start_lat = stold(string_start_lat);
 		sissetulev_rida = sissetulev_rida.substr(sissetulev_rida.find(",", 0) + 1, sissetulev_rida.length()); // new string
 
-		passenger.start_lng =
+		string_start_lng =
 			sissetulev_rida.substr(0, sissetulev_rida.find(",", 0));
-
+		passenger.start_lng = stold(string_start_lng);
 		sissetulev_rida = sissetulev_rida.substr(sissetulev_rida.find(",", 0) + 1, sissetulev_rida.length()); // new string
 
-		passenger.end_lat =
+		string_end_lat =
 			sissetulev_rida.substr(0, sissetulev_rida.find(",", 0));
-
+		passenger.end_lat = stold(string_end_lat);
 		sissetulev_rida = sissetulev_rida.substr(sissetulev_rida.find(",", 0) + 1, sissetulev_rida.length()); // new string
 
-		passenger.end_lng =
+		string_end_lng =
 			sissetulev_rida.substr(0, sissetulev_rida.find(",", 0));
-
+		passenger.end_lng = stold(string_end_lng);
 		sissetulev_rida = sissetulev_rida.substr(sissetulev_rida.find(",", 0) + 1, sissetulev_rida.length()); // new string
 
-		passenger.ride_value =
+		string_ride_value =
 			sissetulev_rida.substr(0, sissetulev_rida.find(",", 0));
-
+		passenger.ride_value = stold(string_ride_value);
 		sissetulev_rida = sissetulev_rida.substr(sissetulev_rida.find(",", 0) + 1, sissetulev_rida.length()); // new string
 
 		epoch_string = time_of_year + "-" + start_time;
@@ -135,7 +147,10 @@ int passenger_information() {
 		int paev = stoi(str_paev);
 		int tund = stoi(str_tund);
 		int minut = stoi(str_minut);
-		double sekund = stod(str_sekund);
+		double double_sekund = stod(str_sekund);
+		int int_sekund = double_sekund * 100;
+
+		double sekund = int_sekund / 100.0;
 
 		int leap_years = 99;
 
@@ -166,69 +181,69 @@ int passenger_information() {
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400;
 
 		}
-		if (int kuu = 2) {
+		else if (int kuu = 2) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 31 * 86400;
 
 		}
-		if (int kuu = 3) {
+		else if (int kuu = 3) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 59 * 86400;
 
 		}
-		if (int kuu = 4) {
+		else if (int kuu = 4) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 90 * 86400;
 
 		}
-		if (int kuu = 5) {
+		else if (int kuu = 5) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 120 * 86400;
 
 		}
-		if (int kuu = 6) {
+		else if (int kuu = 6) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 151 * 86400;
 
 		}
-		if (int kuu = 7) {
+		else if (int kuu = 7) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 181 * 86400;
 
 		}
-		if (int kuu = 8) {
+		else if (int kuu = 8) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 212 * 86400;
 
 		}
-		if (int kuu = 9) {
+		else if (int kuu = 9) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 243 * 86400;
 
 		}
-		if (int kuu = 10) {
+		else if (int kuu = 10) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 273 * 86400;
 
 		}
-		if (int kuu = 11) {
+		else if (int kuu = 11) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 304 * 86400;
 
 		}
-		if (int kuu = 12) {
+		else if (int kuu = 12) {
 
 			passenger.epoch_time = sekund + minut * 60 + tund * 3600 + (paev - 1) * 86400 + (aasta - 1971) * 31536000 + leap_years * 86400 + 334 * 86400;
 
 		}
+		passenger.epoch_time = passenger.epoch_time + timezone * 3600;
+		
+		std::cout << std::setprecision(25) << std::showpoint << std::fixed;
 
+		cout << passenger.epoch_time << "		" << passenger.start_lat << "		" << passenger.start_lng << "		" << passenger.end_lat << "		" << passenger.end_lat << "		" << passenger.ride_value << endl;
 
-		lane_to_print = ("   start lat: " + passenger.start_lat + "   start lng: " + passenger.start_lng + "   end lat: " + passenger.end_lat + "   end lng: " + passenger.end_lng + "  ride value: " + passenger.ride_value);
-		std::cout << std::setprecision(22) << std::showpoint << std::fixed;
-		cout << passenger.epoch_time << endl;
-
-		passengers.push_back(passenger);
-
+		passengers_return.push_back(passenger);
 	}
 
+	return passengers_return;
 }
